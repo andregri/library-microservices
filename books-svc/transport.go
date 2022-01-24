34b5892151d138e8a0894c8b3f64c33fcb683fb2
andregri <andrea.grillo96@live.com>
2022-01-24 23:20:11 +0100

    fix get route accepts only numeric values

--- books-svc/transport.go
+++ books-svc/transport.go
@@ -35,7 +35,7 @@ func MakeHandler(svc BooksService) http.Handler {
 	r := mux.NewRouter()
 
 	r.Handle("/book", postBookHandler).Methods("POST")
-	r.Handle("/book/{id:[a-zA-Z0-9]*}", getBookHandler).Methods("GET")
+	r.Handle("/book/{id:[0-9]*}", getBookHandler).Methods("GET")
 	r.Handle("/book/{id:[0-9]*}", putBookHandler).Methods("PUT")
 	r.Handle("/book/{id:[0-9]*}", deleteBookHandler).Methods("DELETE")
 
